06660aac062514ab7b1e55da238ad45b6cab7228
Spike <florian@playtoday.cc>
2023-11-06 13:59:53 +1100

    New instagram token

--- components/Instagram/Instagram.tsx
+++ components/Instagram/Instagram.tsx
@@ -3,7 +3,7 @@ import { InstagramCarousel } from './instagram-carousel';
 
 export async function Instagram() {
   const data = await fetch(
-    'https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,username,timestamp,permalink,thumbnail_url&access_token=IGQWRNblh3T200TUtfeUFnWHpsYi1vejU2eGc4VU8yNDRhbFNJbDdqRUpFaElFTU9Qc3BMXzNFcW5RWHlTRlFWMG1FZAWRlSHBaazJ3Q0FqMkpES2c1THZAmRmVJV3ZAsNVlpYmlPTVlNM0Q1LXJOMkNzbXNWb2lnWVUZD'
+    `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,username,timestamp,permalink,thumbnail_url&access_token=${process.env.INSTAGRAM_TOKEN}`
   );
   const json = await data.json();
   const instagramPosts: instagramMedia[] = json.data;
--- components/Instagram/instagram-card.tsx
+++ components/Instagram/instagram-card.tsx
@@ -173,7 +173,7 @@ export function InstagramCard({
             <div className="absolute flex justify-center items-center bottom-2 right-2 z-10">
               {clickOnPlay ? (
                 <div
-                  className="w-10 h-10"
+                  className="flex justify-center items-center w-6 h-6 bg-black/60 rounded-full hover:cursor-pointer"
                   onClick={(e) => {
                     e.stopPropagation();
                     if (ref.current) {
@@ -193,13 +193,13 @@ export function InstagramCard({
                       fill="none"
                       viewBox="0 0 24 24"
                       strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
+                      stroke="white"
+                      className="w-4 h-4"
                     >
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
+                        d="M17.25 9.75L19.5 12m0 0l2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6l4.72-4.72a.75.75 0 011.28.531V19.94a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.506-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.395C2.806 8.757 3.63 8.25 4.51 8.25H6.75z"
                       />
                     </svg>
                   ) : (
@@ -208,13 +208,13 @@ export function InstagramCard({
                       fill="none"
                       viewBox="0 0 24 24"
                       strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
+                      stroke="white"
+                      className="w-4 h-4"
                     >
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        d="M17.25 9.75L19.5 12m0 0l2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6l4.72-4.72a.75.75 0 011.28.531V19.94a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.506-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.395C2.806 8.757 3.63 8.25 4.51 8.25H6.75z"
+                        d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
                       />
                     </svg>
                   )}
