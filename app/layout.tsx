ea20a629dc426c58b50e974bc8d1c8c367a10ebd
Spike <florian@playtoday.cc>
2023-12-09 15:03:07 +1100

    Speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { Toaster } from 'react-hot-toast';
 import { Header } from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <Toaster position="top-right" />
+        <SpeedInsights />
       </body>
     </html>
   );
