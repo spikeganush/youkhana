e881bdbe2bb7e182a4a60ec364dcda9f65fa06f5
Spike <florian@playtoday.cc>
2024-12-22 17:13:16 +1100

    Build fix

--- types/general.ts
+++ types/general.ts
@@ -1,7 +1,9 @@
+import { MediaType } from './instagram';
+
 export type InstagramMedia = {
   id: string;
   media_url: string;
-  media_type: string;
+  media_type: MediaType;
   permalink: string;
   caption: string;
   timestamp: string;
