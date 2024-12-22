ce1d2843a1231261cb78cafbde1eb574972b8a40
Spike <florian@playtoday.cc>
2024-12-22 16:20:57 +1100

    Integrated site manifest and updated logo

--- app/seo.config.ts
+++ app/seo.config.ts
@@ -51,7 +51,7 @@ export const defaultMetadata: Metadata = {
     },
   },
   icons: {
-    icon: '/favicon.ico',
+    icon: '/favicon.png',
     shortcut: '/favicon-16x16.png',
     apple: '/apple-touch-icon.png',
   },
